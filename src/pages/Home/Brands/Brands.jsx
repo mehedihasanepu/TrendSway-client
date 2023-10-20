c41d8913e9a29a9aeb12d1e507a246ab66aaa40f
mehedihasanepu <mehedihasanepu123@gmail.com>
2023-10-21 03:52:50 +0600

    login section complete with password validation

--- src/pages/Home/Brands/Brands.jsx
+++ src/pages/Home/Brands/Brands.jsx
@@ -12,18 +12,20 @@ const Brands = () => {
 
     return (
         <div className="mt-10 max-w-screen-xl mx-auto">
-            <h2 className="text-4xl md:text-5xl text-center font-semibold py-6" >Popular Brands</h2>
+            <h2 className="text-4xl md:text-5xl text-center font-semibold  py-6" >Popular Brands</h2>
             <div className="flex justify-between items-center">
 
                 {
                     brands.map(brand => <div key={brand.id} >
 
                         <Link className="p-3" to={`/brandDetails/${brand.id}`}>
-                            <div className="h-24">
-                            <img className="w-32 " src={brand.brand_img} alt="" />
+                            <div className="border-2 px-5 py-10 rounded-2xl bg-white">
+                                <div className="h-24">
+                                    <img className="w-32 " src={brand.brand_img} alt="" />
 
+                                </div>
+                                <h2 className="text-2xl text-center font-bold">{brand.brand_name}</h2>
                             </div>
-                            <h2 className="text-2xl text-center font-bold">{brand.brand_name}</h2>
                         </Link>
                     </div>
                     )
